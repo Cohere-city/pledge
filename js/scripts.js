c9050c7493e9c2f4a8cfdfc03d7b38fb2e274546
Tristan Timblin <tatimblin@gmail.com>
2018-11-09 13:13:13 -0500

    Ajax

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,29 @@
-// SUBMIT TWO FORMS
-let submitForms = function () {
-    document.getElementById("sign").submit();
-    document.getElementById("email").submit();
-}
+// SUBMIT SIGNATURE
+$('.js-form').submit(function () {
+    var form = this;
+
+    //$(form).addClass('form--loading');
+
+    $.ajax({
+      type: $(this).attr('method'),
+      url: $(this).attr('action'),
+      data: $(this).serialize(),
+      contentType: 'application/x-www-form-urlencoded',
+      success: function (data) {
+        console.log('success - ' + data);
+        //showModal('Review submitted', 'Thanks for your review! It will show on the site once it has been approved. You can see the pull request <a href="https://github.com/eduardoboucas/popcorn/pulls">here</a>.');
+        //$(form).removeClass('form--loading');
+      },
+      error: function (err) {
+        console.log(err);
+        //showModal('Error', 'Sorry, there was an error with the submission!');
+        //$(form).removeClass('form--loading');
+      }
+    });
+
+    return false;
+  });
+
 
 // OVERLAY
 const openSign = document.getElementById('openSign')
